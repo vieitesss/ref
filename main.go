772113b5241cc511c158de1543a4ccb7ac490ffa
Daniel Vieites <dvieitest@gmail.com>
2025-03-15 14:13:01 +0100

    feat: get sections with references

--- main.go
+++ main.go
@@ -6,7 +6,19 @@ import (
 	"github.com/gocolly/colly"
 )
 
-var references []string
+type Section struct {
+	Name string
+	References []string
+}
+
+func NewSection(name string, references []string) Section {
+	return Section{
+		Name: name,
+		References: references,
+	}
+}
+
+var sections []Section
 
 func main() {
 	c := colly.NewCollector(
@@ -20,14 +32,17 @@ func main() {
 	c.OnHTML("h2.font-medium", func(e *colly.HTMLElement) {
 		as := e.DOM.NextAllFiltered("div + div.grid").First().ChildrenFiltered("a").EachIter()
 
+		var references []string
 		for _, a := range as {
 			val, _ := a.Attr("href")
 			references = append(references, val)
 		}
+
+		sections = append(sections, NewSection(e.Text, references))
 	})
 
 	c.OnScraped(func(_ *colly.Response) {
-		fmt.Println(references)
+		fmt.Println(sections)
 	})
 
 	c.Visit("https://quickref.me")
