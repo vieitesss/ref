a0aec580279cd011c0b4d62661087bbff8c6c6b4
Daniel Vieites <dvieitest@gmail.com>
2025-03-18 12:47:54 +0100

    feat: show spinners when loading

--- pages/references/references.go
+++ pages/references/references.go
@@ -1,7 +1,10 @@
 package references
 
 import (
+	"fmt"
+
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/londek/reactea"
@@ -16,13 +19,18 @@ type Component struct {
 	reactea.BasicComponent
 	reactea.BasicPropfulComponent[Props]
 
+	loading    bool
+	spinner    spinner.Model
 	list       list.Model
 	references []scraper.Reference
 }
 
 type referencesMsg []scraper.Reference
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
+var (
+	docStyle     = lipgloss.NewStyle().Margin(1, 2)
+	spinnerStyle = lipgloss.NewStyle().Margin(1, 0, 0, 1).Foreground(lipgloss.Color("205"))
+)
 
 func (c *Component) Init(props Props) tea.Cmd {
 	c.UpdateProps(props)
@@ -31,7 +39,14 @@ func (c *Component) Init(props Props) tea.Cmd {
 }
 
 func New() *Component {
-	return &Component{}
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = spinnerStyle
+
+	return &Component{
+		loading: true,
+		spinner: s,
+	}
 }
 
 func getSectionReferencesCmd(section string) tea.Cmd {
@@ -46,6 +61,7 @@ func (c *Component) Update(msg tea.Msg) tea.Cmd {
 	case referencesMsg:
 		c.references = []scraper.Reference(msg)
 		c.list = NewList(c.Props().Section, c.references)
+		c.loading = false
 	case tea.WindowSizeMsg:
 		UpdateSize(&c.list, msg.Width, msg.Height)
 		// case tea.KeyMsg:
@@ -57,11 +73,18 @@ func (c *Component) Update(msg tea.Msg) tea.Cmd {
 		// 	}
 	}
 
-	var cmd tea.Cmd
-	c.list, cmd = c.list.Update(msg)
-	return cmd
+	var cmd_list tea.Cmd
+	var cmd_spinner tea.Cmd
+	c.list, cmd_list = c.list.Update(msg)
+	c.spinner, cmd_spinner = c.spinner.Update(msg)
+
+	return tea.Batch(cmd_list, cmd_spinner)
 }
 
 func (c *Component) Render(int, int) string {
+	if c.loading {
+		return fmt.Sprintf("%v Getting %s references", c.spinner.View(), c.Props().Section)
+	}
+
 	return docStyle.Render(c.list.View())
 }
--- pages/section/section.go
+++ pages/section/section.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/spinner"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/londek/reactea"
@@ -16,31 +17,49 @@ type Component struct {
 	reactea.BasicComponent
 	reactea.BasicPropfulComponent[Props]
 
+	loading  bool
+	spinner  spinner.Model
 	list     list.Model
 	sections []scraper.Section
 }
 
-var docStyle = lipgloss.NewStyle().Margin(1, 2)
+type sectionsMsg []scraper.Section
+
+var (
+	docStyle     = lipgloss.NewStyle().Margin(1, 2)
+	spinnerStyle = lipgloss.NewStyle().Margin(1, 0, 0, 1).Foreground(lipgloss.Color("205"))
+)
 
 func (c *Component) Init(props Props) tea.Cmd {
 	c.UpdateProps(props)
 
-	return nil
+	return getSectionsCmd
 }
 
 func New() *Component {
-	sections := scraper.GetSections()
+	s := spinner.New()
+	s.Spinner = spinner.Dot
+	s.Style = spinnerStyle
 
 	c := &Component{
-		list:     NewList(sections),
-		sections: sections,
+		loading:  true,
+		spinner:  s,
 	}
 
 	return c
 }
 
+func getSectionsCmd() tea.Msg {
+	secs := scraper.GetSections()
+	return sectionsMsg(secs)
+}
+
 func (c *Component) Update(msg tea.Msg) tea.Cmd {
 	switch msg := msg.(type) {
+	case sectionsMsg:
+		c.sections = []scraper.Section(msg)
+		c.list = NewList(c.sections)
+		c.loading = false
 	case tea.WindowSizeMsg:
 		UpdateSize(&c.list, msg.Width, msg.Height)
 	case tea.KeyMsg:
@@ -52,11 +71,18 @@ func (c *Component) Update(msg tea.Msg) tea.Cmd {
 		}
 	}
 
-	var cmd tea.Cmd
-	c.list, cmd = c.list.Update(msg)
-	return cmd
+	var cmd_list tea.Cmd
+	var cmd_spinner tea.Cmd
+	c.list, cmd_list = c.list.Update(msg)
+	c.spinner, cmd_spinner = c.spinner.Update(msg)
+
+	return tea.Batch(cmd_list, cmd_spinner)
 }
 
 func (c *Component) Render(int, int) string {
+	if c.loading {
+		return fmt.Sprintf("%v Getting sections", c.spinner.View())
+	}
+
 	return docStyle.Render(c.list.View())
 }
