6933e8758ad1723d1f46b790f753f6bb3214fcdf
Daniel Vieites <dvieitest@gmail.com>
2025-03-24 14:15:42 +0100

    feat: help in snippets view

--- pages/snippets.go
+++ pages/snippets.go
@@ -1,8 +1,8 @@
 package pages
 
 import (
-	_ "embed"
-
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
@@ -11,8 +11,36 @@ import (
 	"github.com/vieitesss/ref/scraper"
 )
 
-//go:embed file.md
-var in string
+var keys = keyMap{
+	Up: key.NewBinding(
+		key.WithKeys("up", "k"),
+		key.WithHelp("↑/k", "up"),
+	),
+	Down: key.NewBinding(
+		key.WithKeys("down", "j"),
+		key.WithHelp("↓/j", "down"),
+	),
+	HalfUp: key.NewBinding(
+		key.WithKeys("half up", "u"),
+		key.WithHelp("u", "page up"),
+	),
+	HalfDown: key.NewBinding(
+		key.WithKeys("half down", "d"),
+		key.WithHelp("d", "page down"),
+	),
+	Back: key.NewBinding(
+		key.WithKeys("backspace", "b"),
+		key.WithHelp("󰭜/b", "back"),
+	),
+	Help: key.NewBinding(
+		key.WithKeys("?"),
+		key.WithHelp("?", "toggle help"),
+	),
+	Quit: key.NewBinding(
+		key.WithKeys("ctrl+c"),
+		key.WithHelp("ctrl+c", "quit"),
+	),
+}
 
 type SnippetsProps struct {
 	section, reference, title string
@@ -23,11 +51,34 @@ type SnippetsPageMsg SnippetsProps
 // Received when the snippets are available
 type snippetsMsg string
 
+type keyMap struct {
+	Up       key.Binding
+	Down     key.Binding
+	HalfUp   key.Binding
+	HalfDown key.Binding
+	Back     key.Binding
+	Help     key.Binding
+	Quit     key.Binding
+}
+
 type SnippetsPage struct {
 	loading                   bool
 	spinner                   spinner.Model
+	keys                      keyMap
+	help                      help.Model
 	viewport                  viewport.Model
 	section, reference, title string
+}
+
+func (k keyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Up, k.Down, k.HalfUp, k.HalfDown, k.Back, k.Quit, k.Help}
+}
+
+func (k keyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Back, k.Quit, k.Help},
+		{k.Up, k.Down, k.HalfUp, k.HalfDown},
+	}
 }
 
 func NewSnippetsPage(p SnippetsProps) SnippetsPage {
@@ -36,11 +87,13 @@ func NewSnippetsPage(p SnippetsProps) SnippetsPage {
 	s.Style = spinnerStyle
 
 	return SnippetsPage{
+		keys:      keys,
 		loading:   true,
 		spinner:   s,
 		section:   p.section,
 		reference: p.reference,
 		title:     p.title,
+		help:      help.New(),
 	}
 }
 
@@ -64,6 +117,8 @@ func (s SnippetsPage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		width = msg.Width
 		height = msg.Height
 
+		s.help.Width = width
+
 		if s.loading {
 			v := viewport.New(width, height)
 			s.viewport = v
@@ -73,9 +128,22 @@ func (s SnippetsPage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			s.viewport.Width = width
 		}
 
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, s.keys.Help):
+			s.help.ShowAll = !s.help.ShowAll
+		case key.Matches(msg, s.keys.Back):
+			return s, func() tea.Msg {
+				return CheatsheetPageMsg(CheatsheetProps{
+					section:   s.section,
+					reference: s.reference,
+				})
+			}
+		}
+
 	case snippetsMsg:
 		s.loading = false
-		out, _ := glamour.Render(string(msg), "dark")
+		out, _ := glamour.Render(string(msg), "dracula")
 		s.viewport.SetContent(out)
 		return s, nil
 	}
@@ -87,8 +155,16 @@ func (s SnippetsPage) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (s SnippetsPage) View() string {
-	return lipgloss.NewStyle().
-		Height(height).
+	h := lipgloss.NewStyle().
+		Margin(1, 2).
 		Width(width).
+		Align(lipgloss.Left).
+		Render(s.help.View(s.keys))
+
+	vp := lipgloss.NewStyle().
+		Width(width).
+		Align(lipgloss.Top).
 		Render(s.viewport.View())
+
+	return lipgloss.JoinVertical(lipgloss.Top, vp, h)
 }
